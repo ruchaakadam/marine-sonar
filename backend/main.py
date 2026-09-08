0e8f863732f631e374048a84d4b71d1b8795bf71
ruchaakadam <ruchaakadam.17@gmail.com>
2026-09-08 13:55:30 +0530

    Allow live frontend CORS

--- backend/main.py
+++ backend/main.py
@@ -36,6 +36,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
+        "https://marine-sonar-1.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
