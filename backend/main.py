f68aecfb9d067a4516b6b7ac85aa62b2fd992030
ruchaakadam <ruchaakadam.17@gmail.com>
2026-09-11 16:19:30 +0530

    Allow Vercel frontend access

--- backend/main.py
+++ backend/main.py
@@ -35,10 +35,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-        "https://marine-sonar-1.onrender.com",
-    ],
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "https://marine-sonar.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
